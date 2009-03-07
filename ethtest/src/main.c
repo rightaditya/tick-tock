dfcf88dc3b582490fc70c0b52251509d6171c2b7
aditya.bhargava@ualberta.ca <aditya.bhargava@ualberta.ca@8884931c-f708-11dd-8c4e-4bbb75a408b7>
2009-03-07 03:25:43 +0000

    Register settings added, but they still need to be configured properly.

--- ethtest/src/main.c
+++ ethtest/src/main.c
@@ -5,12 +5,47 @@
 #include "parts/m55800/reg_m55800.h"
 
 #define ETH_BASE 0x10000000
+#define ETH_C0_CR 0x00
+#define ETH_C0_ISR 0x04
+#define ETH_GAR 0x80
+#define ETH_SMR 0x84
+#define ETH_SHAR 0x88
+#define ETH_SIPR 0x8E
+
+#define GATEWAY 0x8EF4A19F // 142.244.161.159
+#define SUBNET 0xFFFFFF00 // 255.255.255.0
+#define MACADDR 0x111111111111
+#define IPADDR 0xC0A80040 // 192.168.0.64
 
 void setup();
 
 int main()
 {
+    int *ptri;
+    short *ptrs;
+    char *ptrc;
     setup();
+
+    // set the registers
+    ptri = ETH_BASE + ETH_GAR;
+    *ptri = GATEWAY;
+
+    ptri = ETH_BASE + ETH_SMR;
+    *ptri = SUBNET;
+
+    ptri = ETH_BASE + ETH_SHAR;
+    *ptri = MACADDR & 0x0000FFFFFFFF;
+    ptrs = ETH_BASE + ETH_SHAR + 4;
+    *ptrs = MACADDR >> 4;
+
+    ptrs = ETH_BASE + ETH_SIPR;
+    *ptrs = IPADDR & 0x0000FFFF;
+    ++ptrs;
+    *ptrs = IPADDR >> 4;
+
+    // execute sys_init
+    ptrc = ETH_BASE + ETH_C0_CR;
+    *ptrc = 1;
     
     return 0;
 }
