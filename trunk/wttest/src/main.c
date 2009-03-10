ce05af08b8f6b928b59b6bd35e07ab5105f88e0d
aditya.bhargava@ualberta.ca <aditya.bhargava@ualberta.ca@8884931c-f708-11dd-8c4e-4bbb75a408b7>
2009-03-10 00:51:29 +0000

    Added basic ADC test code.  More or less works.

--- trunk/wttest/src/main.c
+++ trunk/wttest/src/main.c
@@ -2,7 +2,25 @@
 #include <stdio.h>
 #endif
 
+#include "parts/m55800/lib_m55800.h"
+#include "periph/adc/lib_adc.h"
+
 int main()
 {
+    u_int channel[4];
+    
+    at91_adc_open(&ADC0_DESC, ADC_TRGEN_DIS | ADC_10_BIT_RES | ADC_NORMAL_MODE | ADC_PRESCAL);
+    at91_adc_channel_open(&ADC0_DESC, ADC_CH2);
+
+    at91_adc_trig_cmd(&ADC0_DESC, ADC_START);
+
+    while ((at91_adc_get_status(&ADC0_DESC) & ADC_EOC2) == 0) {}
+    at91_adc_read(&ADC0_DESC, channel);
+
+    printf("%d\n", channel[2]);    
+
+    at91_adc_channel_close(&ADC0_DESC, ADC_CH2);
+    at91_adc_close(&ADC0_DESC);
+    
     return 0;
 }
