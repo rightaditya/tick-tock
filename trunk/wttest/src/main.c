85e741b6ba061b2fee3f6e3f373654cf35be164b
aditya.bhargava@ualberta.ca <aditya.bhargava@ualberta.ca@8884931c-f708-11dd-8c4e-4bbb75a408b7>
2009-03-11 23:03:50 +0000

    Something changed.  Not sure what.  ADC code is good though.

--- trunk/wttest/src/main.c
+++ trunk/wttest/src/main.c
@@ -12,12 +12,15 @@ int main()
     at91_adc_open(&ADC0_DESC, ADC_TRGEN_DIS | ADC_10_BIT_RES | ADC_NORMAL_MODE | ADC_PRESCAL);
     at91_adc_channel_open(&ADC0_DESC, ADC_CH2);
 
-    at91_adc_trig_cmd(&ADC0_DESC, ADC_START);
+    while (1)
+    {
+	at91_adc_trig_cmd(&ADC0_DESC, ADC_START);
+	
+	while ((at91_adc_get_status(&ADC0_DESC) & ADC_EOC2) == 0) {}
+	at91_adc_read(&ADC0_DESC, channel);
 
-    while ((at91_adc_get_status(&ADC0_DESC) & ADC_EOC2) == 0) {}
-    at91_adc_read(&ADC0_DESC, channel);
-
-    printf("%d\n", channel[2]);    
+	printf("%d\n", channel[2]);
+    }
 
     at91_adc_channel_close(&ADC0_DESC, ADC_CH2);
     at91_adc_close(&ADC0_DESC);
