5fd43e01c1cbc55633f767186a26af8f0bd6bb10
aditya.bhargava@ualberta.ca <aditya.bhargava@ualberta.ca@8884931c-f708-11dd-8c4e-4bbb75a408b7>
2009-03-12 00:26:10 +0000

    It finally works!

--- ethtest/src/main.c
+++ ethtest/src/main.c
@@ -6,16 +6,20 @@
 
 #define ETH_BASE 0x10000000
 #define ETH_C0_CR 0x00
+#define ETH_C0_CR_SYS_INIT 0x01
+#define ETH_C0_CR_SOCK_INIT 0x02
+#define ETH_C0_CR_CONNECT 0x04
 #define ETH_C0_ISR 0x04
+#define ETH_C0_ISR_INIT_OK 0x01
 #define ETH_GAR 0x80
 #define ETH_SMR 0x84
 #define ETH_SHAR 0x88
 #define ETH_SIPR 0x8E
 
-#define GATEWAY 0x8EF4A19F // 142.244.161.159
-#define SUBNET 0xFFFFFF00 // 255.255.255.0
-#define MACADDR 0x111111111111
-#define IPADDR 0xC0A80040 // 192.168.0.64
+#define GATEWAY 0x0100A8C0 // 192.168.0.1 backwards for big-endian Wiznet
+#define SUBNET 0x00FFFFFF // 255.255.255.0 backwards for big-endian Wiznet
+#define MACADDR 0xB24D4DDB1900 // 00:19:DB:4D:4D:B2 (stolen from Aditya-Desktop) backwards for big-endian wiznet
+#define IPADDR 0x4000A8C0 // 192.168.0.64 backwards for big-endian Wiznet
 
 void setup();
 
@@ -36,16 +40,22 @@ int main()
     ptri = ETH_BASE + ETH_SHAR;
     *ptri = MACADDR & 0x0000FFFFFFFF;
     ptrs = ETH_BASE + ETH_SHAR + 4;
-    *ptrs = MACADDR >> 4;
+    *ptrs = MACADDR >> 32;
 
     ptrs = ETH_BASE + ETH_SIPR;
     *ptrs = IPADDR & 0x0000FFFF;
     ++ptrs;
-    *ptrs = IPADDR >> 4;
+    *ptrs = IPADDR >> 16;
 
     // execute sys_init
     ptrc = ETH_BASE + ETH_C0_CR;
-    *ptrc = 1;
+    *ptrc |= ETH_C0_CR_SYS_INIT;
+
+    /*ptri = ETH_BASE + ETH_C0_ISR;
+    if (*ptri & ETH_C0_ISR_INIT_OK)
+        printf("Initialized OK.\n");
+    else
+        printf("Couldn't initialize!\n");*/
     
     return 0;
 }
@@ -62,3 +72,4 @@ void setup()
     EBI_CSR2 |= EBI_DBW_8;
     EBI_CSR2 |= ETH_BASE;
 }
+
