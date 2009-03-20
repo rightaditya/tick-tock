3f50f1a08aae264e25566ed2bd1062aef28f015e
aditya.bhargava@ualberta.ca <aditya.bhargava@ualberta.ca@8884931c-f708-11dd-8c4e-4bbb75a408b7>
2009-03-20 01:57:03 +0000

    Updated code for new Ethernet module.  Sending out DNS request, not yet receiving or doing HTTP work.

--- ethtest/src/main.c
+++ ethtest/src/main.c
@@ -4,46 +4,18 @@
 #include <string.h>
 
 #include "parts/m55800/reg_m55800.h"
+#include "iinchip_conf.h"
+#include "types.h"
+#include "w5300.h"
 
-#define ETH_BASE 0x10000000
-#define ETH_TX_BUFFER 0x4000
-#define ETH_TX_MASK 0x1FFF
-#define ETH_RX_BUFFER 0x6000
-#define ETH_RX_MASK 0x1FFF
+// TODO: constify
+static uint8 IP[4] = {192, 168, 0, 64};
+static uint8 GW[4] = {192, 168, 0, 1};
+static uint8 SN[4] = {255, 255, 255, 0};
+static uint8 MAC[6] = {0x00, 0x08, 0xDC, 0x00, 111, 200};
 
-#define ETH_GAR 0x80 /* to 0x83 */
-#define ETH_SMR 0x84 /* to 0x87 */
-#define ETH_SHAR 0x88 /* to 0x8D */
-#define ETH_SIPR 0x8E /* to 0x91 */
-
-#define ETH_C0_CR 0x00
-#define ETH_C0_CR_SYS_INIT 0x01
-#define ETH_C0_CR_SOCK_INIT 0x02
-#define ETH_C0_CR_CONNECT 0x04
-#define ETH_C0_CR_CLOSE 0x10
-#define ETH_C0_CR_SEND 0x20
-#define ETH_C0_CR_RECV 0x40
-#define ETH_C0_CR_SWRESET 0x80
-
-#define ETH_C0_SOPR 0xA1
-#define ETH_SOPR_CLOSED 0x0
-#define ETH_SOPR_SOCK_STREAM 0x01
-#define ETH_SOPR_SOCK_DGRAM 0x02
-
-#define ETH_RMSR 0x95
-#define ETH_RMSR_CH0_8KB 0x03
-
-#define ETH_TMSR 0x96
-#define ETH_TMSR_CH0_8KB 0x03
-
-#define ETH_C0_TW_PR 0x40
-#define ETH_C0_TA_PR 0x18
-#define ETH_C0_TR_PR 0x44
-
-#define ETH_C0_DIR 0xA8 /* to 0xAB */
-#define ETH_C0_DPR 0xAC /* to 0xAD */
-#define ETH_C0_SPR 0xA3 /* to 0xAF */
-
+#define MAX_DNS_LENGTH 512
+#define DNS_PORT 53
 #define DNS_HEADER_ID 0x10AB
 #define DNS_HEADER_QR ((0x0 << 15) & 0x8000) /* query */
 #define DNS_HEADER_OPCODE ((0x0 << 11) & 0x7800) /* standard query */
@@ -61,168 +33,91 @@
 #define DNS_QUERY_QTYPE 0x01 /* get the IPv4 address */
 #define DNS_QUERY_QCLASS 0x01 /* we are on the Internet */
 
-#define GATEWAY 0xC0A80001 // 192.168.0.1
-#define SUBNET 0xFFFFFF00 // 255.255.255.0
-#define MACADDR 0x0019DB4D4DB2 // 00:19:DB:4D:4D:B2 (stolen from Aditya-Desktop)
-#define IPADDR 0xC0A80040 // 192.168.0.64
+uint8 bigEndianFirst(uint16 val);
 
-unsigned short endianSwap16(unsigned short x);
-
-unsigned int endianSwap32(unsigned int x);
-
-unsigned long long endianSwap64(unsigned long long x);
-
-void initialize(unsigned int gateway, unsigned int subnet, unsigned long long macaddr, unsigned int ipaddr);
+uint8 bigEndianSecond(uint16 val);
 
 void setup();
 
 int main()
 {
-    unsigned char *ptrc;
-    unsigned short *ptrs;
-    unsigned int *ptri;
+    uint8 txMemConf = 64, rxMemConf = 64;
+    uint8 dnsbuffer[MAX_DNS_LENGTH]; uint8 i = 0;
+    uint16 *dnsbuffer16 = (uint16*)dnsbuffer;
+
+    memset(dnsbuffer, 0, MAX_DNS_LENGTH);
     
     setup();
+
+    iinchip_init();
+
+    if (!sysinit(&txMemConf, &rxMemConf))
+    {
+	printf("Memory configuration error.\n");
+	exit(1);
+    }
+
+    setMR(getMR() | MR_FS);
+
+    setSHAR(MAC);
+    setGAR(GW);
+    setSUBR(SN);
+    setSIPR(IP);
+
+    // TODO: this is for testing only, get rid of it
+    getSHAR(MAC);
+    getGAR(GW);
+    getSUBR(SN);
+    getSIPR(IP);
+
+    socket(0, Sn_MR_UDP, DNS_PORT, 0);
+
+    dnsbuffer16[i++] = DNS_HEADER_ID;
+
+    dnsbuffer16[i++] = DNS_HEADER_QR + DNS_HEADER_OPCODE + DNS_HEADER_AA + DNS_HEADER_TC + DNS_HEADER_RD + DNS_HEADER_RA + DNS_HEADER_Z + DNS_HEADER_RCODE;
+
+    dnsbuffer16[i++] = DNS_HEADER_QDCOUNT;
+
+    dnsbuffer16[i++] = DNS_HEADER_ANCOUNT;
     
-    initialize(GATEWAY, SUBNET, MACADDR, IPADDR);
+    dnsbuffer16[i++] = DNS_HEADER_NSCOUNT;
 
-    ptrc = ETH_BASE + ETH_C0_SOPR;
-    *ptrc = ETH_SOPR_SOCK_DGRAM;
+    dnsbuffer16[i++] = DNS_HEADER_ARCOUNT;
 
-    ptri = ETH_BASE + ETH_C0_DIR;
-    memmove(ptri, ETH_BASE + ETH_GAR, 4);
+    strcpy(&(dnsbuffer[i <<= 1]), DNS_QUERY_QNAME);
+    i += strlen(DNS_QUERY_QNAME) + 1;
 
-    ptrs = ETH_BASE + ETH_C0_DPR;
-    *ptrs = 53;
+    dnsbuffer[i++] = bigEndianFirst(DNS_QUERY_QTYPE);
+    dnsbuffer[i++] = bigEndianSecond(DNS_QUERY_QTYPE);
 
-    ptrs = ETH_BASE + ETH_C0_SPR;
-    *ptrs = 44532;
+    dnsbuffer[i++] = bigEndianFirst(DNS_QUERY_QCLASS);
+    dnsbuffer[i++] = bigEndianSecond(DNS_QUERY_QCLASS);
 
-    ptrc = ETH_BASE + ETH_C0_CR;
-    *ptrc = ETH_C0_CR_SOCK_INIT;
-
-    ptrs = ETH_BASE + ETH_TX_BUFFER;
-    *ptrs = endianSwap16(DNS_HEADER_ID);
-
-    /*++ptrs;
-    *ptri += 2;
-    
-    *ptrs = endianSwap16(DNS_HEADER_QR + DNS_HEADER_OPCODE + DNS_HEADER_AA + DNS_HEADER_TC + DNS_HEADER_RD + DNS_HEADER_RA + DNS_HEADER_Z + DNS_HEADER_RCODE);
-
-    ++ptrs;
-    *ptri += 2;
-    
-    *ptrs = endianSwap16(DNS_HEADER_QDCOUNT);
-
-    ++ptrs;
-    *ptri += 2;
-
-    *ptrs = endianSwap16(DNS_HEADER_ANCOUNT);
-
-    ++ptrs;
-    *ptri += 2;
-    
-    *ptrs = endianSwap16(DNS_HEADER_NSCOUNT);
-
-    ++ptrs;
-    *ptri += 2;
-    
-    *ptrs = endianSwap16(DNS_HEADER_ARCOUNT);
-
-    ++ptrs;
-    *ptri += 2;
-    
-    ptrc = ptrs;
-    strcpy(ptrc, DNS_QUERY_QNAME);
-
-    ptrs += strlen(DNS_QUERY_QNAME) % 2 == 0 ? strlen(DNS_QUERY_QNAME) + 2 : strlen(DNS_QUERY_QNAME) + 1;
-    *ptri += strlen(DNS_QUERY_QNAME) % 2 == 0 ? strlen(DNS_QUERY_QNAME) + 2 : strlen(DNS_QUERY_QNAME) + 1;
-    
-    *ptrs = endianSwap16(DNS_QUERY_QTYPE);
-
-    ++ptrs;
-    *ptri += 2;
-    
-    *ptrs = endianSwap16(DNS_QUERY_QCLASS);*/
+    sendto(0, dnsbuffer, i, GW, DNS_PORT);
 
     return 0;
-}
-
-unsigned short endianSwap16(unsigned short x)
-{
-    return (x << 8) | (x >> 8);
-}
-
-unsigned int endianSwap32(unsigned int x)
-{
-    return (x >> 24) | 
-           ((x << 8) & 0x00FF0000) |
-           ((x >> 8) & 0x0000FF00) |
-           (x << 24);
-}
-
-unsigned long long endianSwap64(unsigned long long x)
-{
-    return (x >> 56) | 
-           ((x << 40) & 0x00FF000000000000) |
-           ((x << 24) & 0x0000FF0000000000) |
-           ((x << 8)  & 0x000000FF00000000) |
-           ((x >> 8)  & 0x00000000FF000000) |
-           ((x >> 24) & 0x0000000000FF0000) |
-           ((x >> 40) & 0x000000000000FF00) |
-           (x << 56);
-}
-
-void initialize(unsigned int gateway, unsigned int subnet, unsigned long long macaddr, unsigned int ipaddr)
-{
-    unsigned int *ptri;
-    unsigned short *ptrs;
-    unsigned char *ptrc;
-
-    /* convert to big-endian for the Wiznet module */
-    gateway = endianSwap32(gateway);
-    subnet = endianSwap32(subnet);
-    macaddr = endianSwap64(macaddr) & 0x0000FFFFFFFFFFFF;
-    ipaddr = endianSwap32(ipaddr);
-    
-    /* now set up the Ethernet module */
-    ptri = ETH_BASE + ETH_GAR;
-    *ptri = gateway;
-
-    ptri = ETH_BASE + ETH_SMR;
-    *ptri = subnet;
-
-    ptri = ETH_BASE + ETH_SHAR;
-    *ptri = macaddr & 0x0000FFFFFFFF;
-    ptrs = ETH_BASE + ETH_SHAR + 4;
-    *ptrs = macaddr >> 32;
-
-    ptrs = ETH_BASE + ETH_SIPR;
-    *ptrs = ipaddr & 0x0000FFFF;
-    ++ptrs;
-    *ptrs = ipaddr >> 16;
-
-    // execute sys_init
-    ptrc = ETH_BASE + ETH_C0_CR;
-    *ptrc = ETH_C0_CR_SYS_INIT;
-
-    ptrc = ETH_BASE + ETH_RMSR;
-    *ptrc = ETH_RMSR_CH0_8KB;
-
-    ptrc = ETH_BASE + ETH_TMSR;
-    *ptrc = ETH_TMSR_CH0_8KB;
 }
 
 void setup()
 {
     EBI_CSR2 = 0x00000000;
     EBI_CSR2 |= EBI_CSEN;
-    EBI_CSR2 |= EBI_BAT_BYTE_WRITE;
+    EBI_CSR2 |= EBI_BAT_BYTE_SELECT;
     EBI_CSR2 |= EBI_TDF_1;
     EBI_CSR2 |= EBI_PAGES_1M;
     EBI_CSR2 |= EBI_WSE;
-    EBI_CSR2 |= EBI_NWS_6;
-    EBI_CSR2 |= EBI_DBW_8;
-    EBI_CSR2 |= ETH_BASE;
+    EBI_CSR2 |= EBI_NWS_8;
+    EBI_CSR2 |= EBI_DBW_16;
+    EBI_CSR2 |= __DEF_IINCHIP_MAP_BASE__;
+}
+
+uint8 bigEndianFirst(uint16 val)
+{
+    return val & 0x00FF;
+}
+
+uint8 bigEndianSecond(uint16 val)
+{
+    return val >> 8;
 }
 
