9bdbde33e52ea2c650cd6d9eeaab4f927ba2c8b0
aditya.bhargava@ualberta.ca <aditya.bhargava@ualberta.ca@8884931c-f708-11dd-8c4e-4bbb75a408b7>
2009-04-03 10:08:57 +0000

    Everything works if using Kienan and Dave's PCB.

--- ethtest/src/main.c
+++ ethtest/src/main.c
@@ -29,43 +29,54 @@
 #define DNS_QUERY_QTYPE 0x01 /* get the IPv4 address */
 #define DNS_QUERY_QCLASS 0x01 /* we are on the Internet */
 
-#define GOOGLE_DNS_NAME "\3www\6google\3com" /* string literal automatically has \0 at the end */
-#define NTP_SERVER_DNS_NAME "\2ca\4pool\3ntp\3org"
+#define NTP_SERVER_DNS_NAME "\4time\3srv8ualberta\2ca" /* string literal automatically has \0 at the end */
+#define GOOGLE_DNS_NAME "\3www\6google\3com"
 
+#define NTP_SERVER_DNS_IP_OFFSET 0x46
 #define GOOGLE_DNS_IP_OFFSET 0x40
-#define NTP_SERVER_DNS_IP_OFFSET 0x2D
+#define ECE_DNS_IP_OFFSET 0x43
 
 #define MAX_NTP_LENGTH 48
 #define NTP_PORT 123
 #define NTP_CLIENT_PORT (NTP_PORT << 9)
 
-#define MAX_HTTP_LENGTH (10 << 10)
+#define MAX_HTTP_LENGTH (20 << 10)
 #define HTTP_PORT 80
 #define HTTP_CLIENT_PORT (HTTP_PORT << 9)
-#define HTTP_HEADER "GET /calendar/feeds/ticktock490%40gmail.com/private-cfbbae074cdf44d7eab836cee95940ad/basic"
+#define HTTP_HEADER_PRE_EMAIL "GET /calendar/feeds/"
+#define HTTP_HEADER_POST_EMAIL "/private-"
+#define HTTP_HEADER_POST_COOKIE "/basic HTTP/1.1\r\nHOST: www.google.com\r\n\r\n"
 
-uint8 *dns(char *dnsname, int ipoffset);
-char *gdata(uint8 *servip);
+#define GMAIL_EMAIL "ticktock490%40gmail.com"
+#define GMAIL_MAGIC_COOKIE "cfbbae074cdf44d7eab836cee95940ad"
+
+#define ALARM_ACTIVE_STRING "Alarm Active"
+
+void dns(char *dnsname, int ipoffset, uint8 *servip);
+int gcal(uint8 *servip, char *gmail, char *password, char *databuffer); /* for now, password is magic cookie */
 unsigned int ntp(uint8 *servip);
    
 static uint8 ip[4] = {192, 168, 0, 101};                   // for setting SIP register
 static uint8 gw[4] = {192, 168, 0, 1};                     // for setting GAR register
 static uint8 sn[4] = {255, 255, 255, 0};                     // for setting SUBR register
 static uint8 mac[6] = {0x00, 0x08, 0xDC, 0x00, 111, 200};      // for setting SHAR register
-static uint8 LAPTOP[4] = {192, 168, 0, 100};
 
 int main()
 {
     uint8 tx_mem_conf[8] = {8, 8, 8, 8, 8, 8, 8, 8};          // for setting TMSR regsiter
     uint8 rx_mem_conf[8] = {8, 8, 8, 8, 8, 8, 8, 8};          // for setting RMSR regsiter
-    uint8 *servip, count = 0;
-    uint8 databuf[MAX_DNS_LENGTH];
-    uint8 defaultgoogle[4] = {74, 125, 155, 104};
-    //uint8 defaultntp[4] = {206, 248, 142, 218};
+    char ntpdnsfixed[22]; /* the \8 doesn't register properly, so need to fix it */
+    uint8 defaultntp[4] = {129, 128, 5, 210};
+    uint8 defaultgoogle[4] = {74, 125, 53, 103};
+    uint8 servip[4];
     unsigned int secs = 0;
-    char *gdatastuff;
+    char gdata[MAX_HTTP_LENGTH];
+    int gdatasize = 0;
+    char *sptr;
 
-    memset(databuf, 0, MAX_DNS_LENGTH);
+    memmove(ntpdnsfixed, NTP_SERVER_DNS_NAME, 22); ntpdnsfixed[9] = 8;
+    memset(servip, 0, 4);
+    memset(gdata, 0, MAX_HTTP_LENGTH);
 
     /* Set up the Wiznet on the EBI */
     EBI_CSR2 = 0x00000000;
@@ -95,38 +106,120 @@ int main()
     setSUBR(sn); // set subnet mask address
     setSIPR(ip); // set source IP address
 
-    /*servip = dns(NTP_SERVER_DNS_NAME, NTP_SERVER_DNS_IP_OFFSET);      
+    dns(ntpdnsfixed, NTP_SERVER_DNS_IP_OFFSET, servip);
 
-    if (servip == NULL)
-	servip = defaultntp;
-    else if (servip[0] == 0)
-	servip = defaultntp;
+    printf("Returned IP: %d.%d.%d.%d\n", servip[0], servip[1], servip[2], servip[3]);    
 
-    printf("Returned IP: %d.%d.%d.%d\n", servip[0], servip[1], servip[2], servip[3]);*/
+    if (servip[0] == 0)
+	memmove(servip, defaultntp, 4);
 
-    secs = ntp(LAPTOP);
+    secs = ntp(servip);
 
     printf("UTC time in seconds: %u\n", secs);
 
-    servip = dns(GOOGLE_DNS_NAME, GOOGLE_DNS_IP_OFFSET);
-
-    if (servip == NULL)
-	servip = defaultgoogle;
-    else if (servip[0] == 0)
-	servip = defaultgoogle;
+    dns(GOOGLE_DNS_NAME, GOOGLE_DNS_IP_OFFSET, servip);
 
     printf("Returned IP: %d.%d.%d.%d\n", servip[0], servip[1], servip[2], servip[3]);
 
-    /* Now we have the time and Google's IP.  GData time. */
+    if (servip[0] == 0)
+	memmove(servip, defaultgoogle, 4);
 
-    gdatastuff = gdata(servip);
+    gdatasize = gcal(servip, GMAIL_EMAIL, GMAIL_MAGIC_COOKIE, gdata);
+
+    if (gdatasize < 0)
+	printf("Got too many bytes from Google!\n");
+    else
+    {
+	printf("Got %d bytes from Google\n", gdatasize);
+    
+	sptr = strstr(gdata, ALARM_ACTIVE_STRING);
+    
+	while (sptr != NULL)
+	{
+	    int month = 0, day = 0, year = 0, starth = 0, endh = 0, startm = 0, endm = 0;
+	    char *monthptr, *sptr2;
+	    
+	    sptr = strstr(sptr, "When: ");
+
+	    monthptr = sptr + 10;
+
+	    if (strncmp(monthptr, "Jan", 3) == 0)
+		month = 1;
+	    else if (strncmp(monthptr, "Feb", 3) == 0)
+		month = 2;
+	    else if (strncmp(monthptr, "Mar", 3) == 0)
+		month = 3;
+	    else if (strncmp(monthptr, "Apr", 3) == 0)
+		month = 4;
+	    else if (strncmp(monthptr, "May", 3) == 0)
+		month = 5;
+	    else if (strncmp(monthptr, "Jun", 3) == 0)
+		month = 6;
+	    else if (strncmp(monthptr, "Jul", 3) == 0)
+		month = 7;
+	    else if (strncmp(monthptr, "Aug", 3) == 0)
+		month = 8;
+	    else if (strncmp(monthptr, "Sep", 3) == 0)
+		month = 9;
+	    else if (strncmp(monthptr, "Oct", 3) == 0)
+		month = 10;
+	    else if (strncmp(monthptr, "Nov", 3) == 0)
+		month = 11;
+	    else if (strncmp(monthptr, "Dec", 3) == 0)
+		month = 12;
+	    
+	    day = atoi(sptr + 14);
+
+	    sptr = strchr(sptr, ',');
+
+	    year = atoi(sptr + 2);
+	    starth = atoi(sptr + 7);
+
+	    sptr2 = strchr(sptr, ':');
+	    if (sptr2 != NULL && (sptr2 < strstr(sptr, "to")))
+		startm = atoi(sptr2 + 1);
+
+	    sptr = strstr(sptr, "to");
+
+	    endh = atoi(sptr + 3);
+
+	    sptr2 = strchr(sptr, ':');
+	    if (sptr2 != NULL)
+		endm = atoi(sptr2 + 1);
+
+	    if (strncmp(sptr - 3, "pm", 2) == 0)
+		starth += 12;
+
+	    sptr = strstr(sptr, "&amp;nbsp;");
+
+	    if (strncmp(sptr - 2, "pm", 2) == 0)
+		endh += 12;
+
+	    if (starth % 12 == 0)
+		starth -= 12;
+	    if (endh % 12 == 0)
+		endh -= 12;
+
+	    sptr = strstr(sptr, ALARM_ACTIVE_STRING);
+
+	    printf("Month: %d\n", month);
+	    printf("Day: %d\n", day);
+	    printf("Year: %d\n", year);
+	    printf("Starth: %d\n", starth);
+	    printf("Startm: %d\n", startm);
+	    printf("Endh: %d\n", endh);
+	    printf("Endm: %d\n", endm);
+
+	    /* TODO: save */
+	}
+    }
 
     return 0;
 }
 
-uint8 *dns(char *dnsname, int ipoffset)
+void dns(char *dnsname, int ipoffset, uint8 *servip)
 {
-    uint8 i = 0, servip[4], dnsbuffer[MAX_DNS_LENGTH];
+    uint8 i = 0, dnsbuffer[MAX_DNS_LENGTH];
     uint16 *dnsbuffer16 = (uint16 *)dnsbuffer;
     uint32 len;
     int received = 0;
@@ -169,15 +262,12 @@ uint8 *dns(char *dnsname, int ipoffset)
     close(0);
 
     memmove(servip, dnsbuffer + ipoffset, 4);
-
-    return servip;
 }
 
-char *gdata(uint8 *servip)
+int gcal(uint8 *servip, char *gmail, char *password, char *databuffer)
 {
-    uint8 databuffer[MAX_HTTP_LENGTH];
-    uint32 len, i = 0;
-    int received = 0;
+    uint32 len;
+    int i = 0, count = 0;
 
     close(1);
     socket(1, Sn_MR_TCP, HTTP_CLIENT_PORT, 0);
@@ -188,24 +278,45 @@ char *gdata(uint8 *servip)
 
     memset(databuffer, 0, MAX_HTTP_LENGTH);
 
-    strcpy(databuffer, HTTP_HEADER);
-    i += strlen(HTTP_HEADER);
+    strcpy(databuffer, HTTP_HEADER_PRE_EMAIL);
+    i += strlen(HTTP_HEADER_PRE_EMAIL);
+
+    strcpy(databuffer + i, gmail);
+    i += strlen(gmail);
+
+    strcpy(databuffer + i, HTTP_HEADER_POST_EMAIL);
+    i += strlen(HTTP_HEADER_POST_EMAIL);
+
+    strcpy(databuffer + i, password);
+    i += strlen(password);
+
+    strcpy(databuffer + i, HTTP_HEADER_POST_COOKIE);
+    i += strlen(HTTP_HEADER_POST_COOKIE);
+
+    wait_10ms(10);
 
     send(1, databuffer, i);
 
-    while (!received)
+    i = 0;
+    while (count < 10 && i >= 0)
     {
 	if ((len = getSn_RX_RSR(1)) > 0)
 	{
-	    len = recv(1, databuffer, len);
-	    received = 1;
+	    if ((i + len) > MAX_HTTP_LENGTH)
+		i = -1;
+	    else
+		i += recv(1, databuffer + i, len);
 	}
+	else
+	    ++count;
+
+	wait_10ms(10);
     }
     
     disconnect(1);
     close(1);
 
-    return 1;
+    return i;
 }
 
 unsigned int ntp(uint8 *servip)
